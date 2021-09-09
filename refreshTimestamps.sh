1de8add17e8d0f8e0e3c1fa8f6b5d0480123f7e4
huss2021 <85590639+huss2021@users.noreply.github.com>
2021-09-09 12:52:57 -0500

    reset with more realistic dates

--- refreshTimestamps.sh
+++ refreshTimestamps.sh
@@ -3,8 +3,9 @@
 # requires the gsed (GNU sed) command to be installed on mac through homebrw
 # Jonathan Tomasulo - Cybersecurity Specialist
 
-currentDate=`date '+%FT%T.000Z'`
-futureDate='"last_observed": "2021-09-09T15:49:50.000Z'
+#currentDate=`date '+%FT%T.000Z'`
+futureDate='"last_observed": "2021-09-09T01:55:00.000Z'
+currentDate='2021-09-09T00:00:00.000Z'
 echo "$currentDate"
 gsed -i "s/[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]T[0-9][0-9]:[0-9][0-9]:[0-9][0-9]\.[0-9][0-9][0-9]Z/$currentDate/g" *.json
 gsed -i "s/\"last_observed\": \"[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]T[0-9][0-9]:[0-9][0-9]:[0-9][0-9]\.[0-9][0-9][0-9]Z/$futureDate/g" *.json
